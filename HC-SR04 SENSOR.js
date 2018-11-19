0cba2fa4b10a1b06dd54369766ef3fef75dcb2d4
Muh Yusuf <myusuf1982@gmail.com>
2018-11-19 09:23:49 +0700

    Update HC-SR04 SENSOR.js

--- HC-SR04 SENSOR.js
+++ HC-SR04 SENSOR.js
@@ -1,6 +1,7 @@
 var rpio = require('rpio');
 
-var GPIO_TRIGGER = 33 var GPIO_ECHO = 35
+var GPIO_TRIGGER = 33 
+var GPIO_ECHO = 35
 
 rpio.open(GPIO_TRIGGER, rpio.OUTPUT, rpio.LOW); rpio.open(GPIO_ECHO, rpio.INPUT);
 
@@ -10,7 +11,9 @@ var k=0;
 
 while( k==0) { rpio.msleep(2000);
 
-rpio.write(GPIO_TRIGGER, rpio.HIGH); rpio.usleep(10); rpio.write(GPIO_TRIGGER, rpio.LOW);
+rpio.write(GPIO_TRIGGER, rpio.HIGH); 
+rpio.usleep(10);
+rpio.write(GPIO_TRIGGER, rpio.LOW);
 
 while (rpio.read(GPIO_ECHO) == 0) { var hrTime = process.hrtime() StartTime = ((hrTime[0] * 1000000 + hrTime[1] / 1000)/1000000) }
 
